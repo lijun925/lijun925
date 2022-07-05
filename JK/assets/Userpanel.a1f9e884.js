c1d6b09e91cd49f9c9a06751754892a9caa987d8
lijun925 <106026790+lijun925@users.noreply.github.com>
2022-07-05 17:04:40 +0800

    Add files via upload
    JK

---
+++ JK/assets/Userpanel.a1f9e884.js
@@ -0,0 +1,14 @@
+var a={template:`
+      <div class="userp">
+         
+      
+          <div class="userimg">
+            <img src="https://img2.baidu.com/it/u=3210120610,423473844&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"/>
+          </div>
+    
+          <van-notice-bar
+       left-icon="volume-o" color="#1989fa" background="#ecf9ff"
+      text="\u6B64\u9879\u76EE\u56FE\u7247\u7D20\u6750\u5747\u6765\u81EA\u7F51\u7EDC\uFF0C\u4EC5\u4F9B\u5B66\u4E60\u53C2\u8003\uFF0C\u5982\u6709\u7591\u95EE\uFF0C\u8BF7+QQ 1174543895"
+      />
+      </div>
+    `};export{a as default};
